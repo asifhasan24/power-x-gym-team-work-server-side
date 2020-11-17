020eed7132c2d97073da8b52e03d4d9e913428ee
Asif Hasan <67521432+asifhasan24@users.noreply.github.com>
2020-11-17 21:15:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,39 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-require("dotenv").config();
+
 const app = express();
 
 app.use(cors())
 app.use(express())
 
 app.get('/', (req, res) => {
-    res.send(" I am on... ")
+    res.send(" I am ok")
 })
 
 
 const MongoClient = require("mongodb").MongoClient;
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b31bz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://powergym:asif1234@cluster0.gbfwa.mongodb.net/power?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true  });
 client.connect((err) => {
-  const collection =  client .db(`${process.env.DB_NAME}`) .collection(`${process.env.DB_COLL}`);
+  const collection = client.db("power").collection("paydata");
+  const paydata2 = client.db("power").collection("paydata2");
   
    app.get("/ourClasses", (req, res) => {
      collection.find({}).toArray((err, document) => {
        res.send(document);
      });
    });
+
+   app.post('/adddata', (req, res) => {
+  const order = req.body;
+     serviceCollection.insertOne(order)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
  
 });
 
 
 app.listen(5000)
-// app.listen(process.env.PORT || 8000)
